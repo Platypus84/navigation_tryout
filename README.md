80476df94905fd74a8cc5e7f792f7f42d6bbf5d3
Platypus84 <mailtojulian@gmx.de>
2025-07-21 10:58:48 +0200

    task_4.5.4_aufgabe_0

---
+++ README.md
@@ -0,0 +1,3 @@
+# navigation_tryout
+
+A new Flutter project.
---
+++ lib/likes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Likes extends StatelessWidget {
+  const Likes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Like'),
+        Text('Hier findest du deine geliketen Nachrichten.'),
+      ],
+    );
+  }
+}
